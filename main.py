fbbd9cb55ea6e8276d006464f99bbfa9a258ff23
Vincent Buscarello <vince.buscarello@gmail.com>
2016-11-23 15:29:19 -0700

    first step in combining app engine and movie trailers roject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -9,7 +9,13 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Udacity!'
+    return 'Hello Udacity! <a href>Movie site here </a>'
+
+@app.route('/movies')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return render_template('fresh_tomatoes.html')
+
 
 
 @app.errorhandler(404)
